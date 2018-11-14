2e5b7edef80099451ed37e175d71949fcb54ae29
Steven <ekczile@gmail.com>
2018-11-14 14:43:47 -0500

    added solution

--- app.rb
+++ app.rb
@@ -7,16 +7,49 @@ end
 
 post '/number1' do
     number1 = params[:num1]
-    redirect '/operator?fnum1=' + number1
+    redirect '/number2?num1=' + number1
 end
 
-  get '/operator' do
+  get '/number2' do
     number1 = params[:num1]
     erb :number2, locals: {number1: number1}
  end
 
+ post '/number2' do
+    number1 = params[:num1]
+    number2 = params[:num2]
+    redirect '/operator?num1=' + number1 + '&num2=' + number2
+ end
+
+ get '/operator' do
+    number1 = params[:num1]
+    number2 = params[:num2]
+    erb :operator, locals: {number1: number1, number2: number2}
+ end
+ 
  post '/operator' do
     number1 = params[:num1]
     number2 = params[:num2]
-    redirect '/calculation?num1=' + number1 + '&num2=' + number2
+    operator = params[:oper]
+    redirect '/calculation?num1=' + number1 + '&num2=' + number2 + '&oper=' + operator
+ end
+
+ get '/calculation' do
+    number1 = params[:num1].to_i
+    number2 = params[:num2].to_i
+    operator = params[:oper]
+    if operator == "add"
+        solution = addition(number1, number2)
+        operator = "\+"
+    elsif operator == "-"
+        solution = subtract(number1, number2)
+        operator = "-"
+    elsif operator == "multi"
+        solution = multiplication(number1, number2)
+        operator = "*"
+    elsif operator == "div"
+        solution = division(number1, number2)
+        operator = "/"
+    end
+    erb :calculation, locals: {number1: number1, number2: number2, operator: operator, solution: solution}
  end
