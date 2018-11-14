bc29971d3149e41202f7ac43967d3363f571e913
Steven <ekczile@gmail.com>
2018-11-14 11:01:24 -0500

    added 2nd number page

--- app.rb
+++ app.rb
@@ -7,5 +7,16 @@ end
 
 post '/number1' do
     number1 = params[:num1]
-    redirect '/number2?fnum1=' + number1
-   end
+    redirect '/operator?fnum1=' + number1
+end
+
+  get '/operator' do
+    number1 = params[:num1]
+    erb :number2, locals: {number1: number1}
+ end
+
+ post '/operator' do
+    number1 = params[:num1]
+    number2 = params[:num2]
+    redirect '/calculation?num1=' + number1 + '&num2=' + number2
+ end
